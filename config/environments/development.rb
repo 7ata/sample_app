43fb8739572be996b73886a4be0856629a43aa0b
7ata <103552711+7ata@users.noreply.github.com>
2022-06-09 10:29:22 +0000

    [Finish] 3-1

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.hosts << "97f8fbb6475542989783655212fc1d41.vfs.cloud9.us-east-1.amazonaws.com"
 end
