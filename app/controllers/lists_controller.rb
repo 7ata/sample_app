1a71d03a74a9ff87478984394c3d92d579ccb8d9
7ata <103552711+7ata@users.noreply.github.com>
2022-06-13 15:14:07 +0000

    [add] 3-7

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -3,29 +3,34 @@ class ListsController < ApplicationController
     # Viewへ渡すためのインスタンス変数に空のModelオブジェクトを生成する。
     @list = List.new
   end
-
-  # 以下を追加
+  
   def create
     # １.&2. データを受け取り新規登録するためのインスタンス作成
     list = List.new(list_params)
     # 3. データをデータベースに保存するためのsaveメソッド実行
     list.save
-    # 4. トップ画面へリダイレクト
     # redirect_to '/top' を削除して、以下コードに変更
     # 詳細画面へリダイレクト
     redirect_to list_path(list.id)
   end
-
+  
+  def index
+    @lists = List.all
+  end
+  
   def show
     @list = List.find(params[:id])
   end
 
-  def index
-    @lists = List.all
-  end
-
   def edit
     @list = List.find(params[:id])
+  end
+
+
+  def destroy
+    list = List.find(params[:id])  # データ（レコード）を1件取得
+    list.destroy  # データ（レコード）を削除
+    redirect_to '/lists'  # 投稿一覧画面へリダイレクト
   end
 
   def update
